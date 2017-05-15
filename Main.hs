0556cb5c72c9b10946decf307c497db4fa767cdf
Dave Compton <davecompton7@gmail.com>
2017-05-15 16:46:14 +0000

    major rename from row/col to x/y

--- Main.hs
+++ Main.hs
@@ -18,54 +18,54 @@ percolateR [] (Field f h v) = (Field f h v, [])
 percolateR seep (Field f h v) = percolateR   
                        (concat $ fmap neighbors validSeep) 
                        (Field (f // map (\p -> (p,'.')) validSeep) h v) where
-    north (r,c) = [(r-1,c)]
-    south (r,c) = [(r+1,c)]
-    west (r,c) =  [(r, c-1)]
-    east (r,c) =  [(r, c+1)]
-    neighbors (r,c) = north(r,c) ++ south(r,c) ++ west(r,c) ++ east(r,c)
-    ((rLo,cLo),(rHi,cHi)) = bounds f
-    validSeep = filter (\p@(r,c) -> r >= rLo && 
-                                    r <= rHi && 
-                                    c >= cLo && 
-                                    c <= cHi && 
+    north (x,y) = [(x,y-1)]
+    south (x,y) = [(x,y+1)]
+    west (x,y) =  [(x-1,y)]
+    east (x,y) =  [(x+1,y)]
+    neighbors (x,y) = north(x,y) ++ south(x,y) ++ west(x,y) ++ east(x,y)
+    ((xLo,yLo),(xHi,yHi)) = bounds f
+    validSeep = filter (\p@(x,y) -> x >= xLo && 
+                                    x <= xHi && 
+                                    y >= yLo && 
+                                    y <= yHi && 
                                     f!p == ' ') $ nub $ sort seep
  
 -- Percolate a field;  Return the percolated field.
 percolate :: Field -> Field
 percolate start@(Field f _ _) = 
-    let ((_,_),(_,cHi)) = bounds f
-        (final, _) = percolateR [(0,c) | c <- [0..cHi]] start
+    let ((_,_),(_,yHi)) = bounds f
+        (final, _) = percolateR [(0,y) | y <- [0..yHi]] start
     in final
  
 -- Generate a random field.
 randomField :: Int -> Int -> Double -> Rand StdGen Field
-randomField rows cols threshold = do
+randomField width height threshold = do
     let toChar t = if (t<threshold) then ' ' else '#'
     rnd <- fmap toChar <$> getRandoms 
-    let f = listArray ((0,0), (rows-1, cols-1)) rnd
+    let f = listArray ((0,0), (width-1, height-1)) rnd
 
     hrnd <- fmap (<threshold) <$> getRandoms
-    let h = listArray ((0,0),(rows-1, cols)) hrnd
+    let h = listArray ((0,0),(width-1, height)) hrnd
 
     vrnd <- fmap (<threshold) <$> getRandoms 
-    let v = listArray ((0,0),(rows, cols-1)) vrnd
+    let v = listArray ((0,0),(width, height-1)) vrnd
 
     return $ Field f h v
  
 -- Assess whether or not percolation reached bottom of field.
 leaky :: Field -> Bool
-leaky (Field f _ _) = '.' `elem` [f!(rHi,c) | c <- [cLo..cHi]] where
-               ((_,cLo),(rHi,cHi)) = bounds f
+leaky (Field f _ _) = '.' `elem` [f!(xHi,y) | y <- [yLo..yHi]] where
+               ((_,yLo),(xHi,yHi)) = bounds f
  
 -- Run test once; Return bool indicating success or failure.
 oneTest :: Int -> Int -> Double -> Rand StdGen Bool
-oneTest rows cols threshold = 
-    leaky <$> percolate <$> randomField rows cols threshold
+oneTest width height threshold = 
+    leaky <$> percolate <$> randomField width height threshold
  
 -- Run test multple times; Return the number of tests that pass
 multiTest :: Int -> Int -> Int -> Double -> Rand StdGen Double
-multiTest repeats rows cols threshold = do
-    results <- replicateM repeats $ oneTest rows cols threshold
+multiTest repeats width height threshold = do
+    results <- replicateM repeats $ oneTest width height threshold
     let leakyCount = length $ filter (==True) results
     return $ fromIntegral leakyCount / fromIntegral repeats
  
@@ -75,8 +75,8 @@ alternate (a:as) bs = a : alternate bs as
  
 showField :: Field -> IO ()
 showField (Field a h v) =  do
-    let ((rLo,cLo),(rHi,cHi)) = bounds a
-        fLines =  map show [ [ a!(r,c) | c <- [cLo..cHi]] | r <- [rLo..rHi]]
+    let ((xLo,yLo),(xHi,yHi)) = bounds a
+        fLines =  map show [ [ a!(x,y) | y <- [yLo..yHi]] | x <- [xLo..xHi]]
     mapM_ putStrLn fLines
 
 
@@ -101,5 +101,5 @@ main = do
                            | n <- ns,
                              let v = fromIntegral n / fromIntegral d ]
   let results = zip ns (evalRand tests g2)
-  mapM_ print [format ("p=" % int % "/" % int % " -> " % fixed 4) n d r | (n,r) <- results]
+  mapM_ print [format ("p=" % int % "/" % int % " -> " % fixed 4) n d x | (n,x) <- results]
  
