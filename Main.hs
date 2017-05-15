b2ab559e12c8a228a86aec3637c9af4192f1b51c
Dave Compton <davecompton7@gmail.com>
2017-05-15 01:38:51 +0000

    using north, south, east, west; initializing random walls ( but not yet using)

--- Main.hs
+++ Main.hs
@@ -5,30 +5,33 @@ import           Data.Array.Unboxed
 import           Data.List
 import           Formatting
  
-data Field = Field (UArray (Int, Int) Char)
+data Field = Field { f :: (UArray (Int, Int) Char)  
+                   , hWall :: (UArray (Int, Int) Bool)  
+                   , vWall :: (UArray (Int, Int) Bool)  
+                   }
+
  
 -- Start percolating some seepage through a field.
 -- Recurse to continue percolation with spreading seepage.
 percolateR :: [(Int, Int)] -> Field -> (Field, [(Int,Int)])
-percolateR [] (Field f) = (Field f, [])
-percolateR seep (Field f) = percolateR   
+percolateR [] (Field f h v) = (Field f h v, [])
+percolateR seep (Field f h v) = percolateR   
                        (concat $ fmap neighbors validSeep) 
-                       (Field (f // map (\p -> (p,'.')) validSeep)) where
-    neighbors (r,c) = [(r-1,c), (r+1,c), (r, c-1), (r, c+1)]
-    north (r,c) = [(r-1,c), (r+1,c), (r, c-1), (r, c+1)]
-    south (r,c) = [(r-1,c), (r+1,c), (r, c-1), (r, c+1)]
-    east (r,c) = [(r-1,c), (r+1,c), (r, c-1), (r, c+1)]
-    west (r,c) = [(r-1,c), (r+1,c), (r, c-1), (r, c+1)]
+                       (Field (f // map (\p -> (p,'.')) validSeep) h v) where
+    north (r,c) = [(r-1,c)]
+    south (r,c) = [(r+1,c)]
+    west (r,c) =  [(r, c-1)]
+    east (r,c) =  [(r, c+1)]
+    neighbors (r,c) = north(r,c) ++ south(r,c) ++ west(r,c) ++ east(r,c)
     ((rLo,cLo),(rHi,cHi)) = bounds f
-    validSeep = filter (\p@(r,c) -> r >= rLo && 
-                                    r <= rHi && 
+    validSeep = filter (\p@(r,c) -> r >= rLo && r <= rHi && 
                                     c >= cLo && 
                                     c <= cHi && 
                                     f!p == ' ') $ nub $ sort seep
  
 -- Percolate a field;  Return the percolated field.
 percolate :: Field -> Field
-percolate start@(Field f) = 
+percolate start@(Field f _ _) = 
     let ((_,_),(_,cHi)) = bounds f
         (final, _) = percolateR [(0,c) | c <- [0..cHi]] start
     in final
@@ -37,14 +40,22 @@ percolate start@(Field f) =
 randomField :: Int -> Int -> Double -> Rand StdGen Field
 randomField rows cols threshold = do
     rnd <- replicateM rows (replicateM cols $ getRandomR (0.0, 1.0))
-    return $ Field $ array ((0,0), (rows-1, cols-1)) 
-                    [((r,c), if rnd !! r !! c < threshold then ' ' 
-                             else '#') 
+    hrnd <- replicateM (rows+1) (replicateM (cols+1) $ getRandomR (0.0, 1.0))
+    vrnd <- replicateM (rows+1) (replicateM (cols+1) $ getRandomR (0.0, 1.0))
+    let f = array ((0,0), (rows-1, cols-1)) 
+                    [((r,c), if rnd !! r !! c < threshold then ' ' else '#') 
                      | r <- [0..rows-1], c <- [0..cols-1] ] 
+    let h = array ((0,0), (rows, cols)) 
+                    [((r,c), hrnd !! r !! c < threshold) 
+                     | r <- [0..rows], c <- [0..cols] ] 
+    let v = array ((0,0), (rows, cols)) 
+                    [((r,c), vrnd !! r !! c < threshold) 
+                     | r <- [0..rows], c <- [0..cols] ] 
+    return $ Field f h v
  
 -- Assess whether or not percolation reached bottom of field.
 leaky :: Field -> Bool
-leaky (Field f) = '.' `elem` [f!(rHi,c) | c <- [cLo..cHi]] where
+leaky (Field f _ _) = '.' `elem` [f!(rHi,c) | c <- [cLo..cHi]] where
                ((_,cLo),(rHi,cHi)) = bounds f
  
 -- Run test once; Return bool indicating success or failure.
@@ -60,7 +71,7 @@ multiTest repeats rows cols threshold = do
     return $ fromIntegral leakyCount / fromIntegral repeats
  
 showField :: Field -> IO ()
-showField (Field a) =   mapM_ print [ [ a!(r,c) | c <- [cLo..cHi]] | r <- [rLo..rHi]]
+showField (Field a _ _) =   mapM_ print [ [ a!(r,c) | c <- [cLo..cHi]] | r <- [rLo..rHi]]
               where ((rLo,cLo),(rHi,cHi)) = bounds a
  
 main :: IO ()
